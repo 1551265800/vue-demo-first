5ef27056b4feb950ce352d0275b0ebfa1f031574
诗昂 <1551265800@qq.com>
2022-09-21 02:10:00 +0800

    index

---
+++ src/components/Count.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h5>Count组件(全局)</h5>
+        <p>Count的值是{{count}}</p>
+        <button @click="add">加1</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                count: this.init
+            }
+        },
+        methods: {
+            add() {
+                this.count++;
+            }
+        },
+        //自定义属性
+        props: {
+            init: {
+                type: Number,
+                default: 0
+            },
+        },
+    }
+</script>
+
+<style lang="less" scoped>
+     
+</style>
